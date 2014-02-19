0c75b64d228326c31d5137b1171febcce67725fd
Basil Gren <bas.gren@gmail.com>
2014-02-19 16:10:03 +0300

    Fix dependencies for PAC Manager.

--- site-cookbooks/rbit_workstation/recipes/pac_manager.rb
+++ site-cookbooks/rbit_workstation/recipes/pac_manager.rb
@@ -1,3 +1,9 @@
+
+# Fix dependencies for Linux Mint
+execute 'apt-fix-dependencies' do
+  command 'apt-get -f install'
+end
+
 remote_file '/tmp/pac-4.5.3.4-all.deb' do
   source 'http://kent.dl.sourceforge.net/project/pacmanager/pac-4.0/pac-4.5.3.4-all.deb'
   mode 0644
