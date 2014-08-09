23b829746fa86b7759fa9d130b41844afff8389a
Basil Gren <bas.gren@gmail.com>
2014-08-10 02:17:09 +0300

    Update cookbook for PAC Manager.

--- site-cookbooks/rbit_workstation/recipes/pac_manager.rb
+++ site-cookbooks/rbit_workstation/recipes/pac_manager.rb
@@ -1,15 +1,15 @@
 
 # Fix dependencies for Linux Mint
 execute 'apt-fix-dependencies' do
-  command 'apt-get -f install'
+  command 'apt-get -f -y install'
 end
 
-remote_file '/tmp/pac-4.5.3.4-all.deb' do
-  source 'http://kent.dl.sourceforge.net/project/pacmanager/pac-4.0/pac-4.5.3.4-all.deb'
+remote_file '/tmp/pac-4.5.4-all.deb' do
+  source 'http://downloads.sourceforge.net/project/pacmanager/pac-4.0/pac-4.5.4-all.deb?r=&ts=1407537011&use_mirror=netcologne'
   mode 0644
 end
 
 dpkg_package 'pac_manager' do
-  source '/tmp/pac-4.5.3.4-all.deb'
+  source '/tmp/pac-4.5.4-all.deb'
   action :install
 end
